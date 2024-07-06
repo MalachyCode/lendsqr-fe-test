6487ffbd660aa6cf9c5b238a53fabb6ef1314915
MalachyCode <nwaformalachy2@gmail.com>
2024-07-06 14:46:57 +0100

    A component directory is created for resusable components. A form input component is created to render the form inputs for the login page.

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,58 @@
+import './formInput.scss';
+import { useState } from 'react';
+import { FormInputType } from '../types';
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    // extends React's HTMLAttributes
+    focused?: string;
+  }
+}
+
+const FormInput = (props: FormInputType) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, type, ...inputProps } = props;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(
+      new RegExp(`${props.regex}`).test(props.value) === true ? false : true
+    );
+  };
+
+  const showVisibilityIcon = () => {
+    return showPassword ? (
+      <span onClick={() => setShowPassword(!showPassword)}>Hide</span>
+    ) : (
+      <span onClick={() => setShowPassword(!showPassword)}>Show</span>
+    );
+  };
+
+  return (
+    <div className='form-input-container'>
+      <div className={'input-box ' + (focused && 'error')}>
+        <input
+          {...inputProps}
+          type={
+            type === 'password' ? (showPassword ? 'text' : 'password') : type
+          }
+          className='form-input'
+          id={id}
+          onChange={onChange}
+          onBlur={handleFocus} //onBlur===click and leave input
+          onFocus={() => {
+            inputProps.name === 'confirmPassword' && setFocused(true);
+          }} //onFocus===click on input
+          focused={focused.toString()}
+        />
+        <span className={'placeholder ' + (props.value && 'active')}>
+          {label}
+        </span>
+        {props.type === 'password' ? showVisibilityIcon() : ''}
+      </div>
+      {focused && <span className='error-message'>{errorMessage}</span>}
+    </div>
+  );
+};
+
+export default FormInput;
