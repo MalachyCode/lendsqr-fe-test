6eb64705cf5baef81dc642f936e4000e15509fde
MalachyCode <nwaformalachy2@gmail.com>
2024-07-08 08:58:33 +0100

    New route created for dashboard page in the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Login from './pages/Login/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </div>
   );
