4dfd860aacfae0b4ee04e5a9bbb7e03d0ac2806f
MalachyCode <nwaformalachy2@gmail.com>
2024-07-09 09:15:40 +0100

    File created to contain data rendered in the dashboard table, including the table headers and dummy body data used to build out the table.

---
+++ src/pages/Dashboard/tableData.ts
@@ -0,0 +1,73 @@
+export const tableHeaders = [
+  {
+    id: 1,
+    name: 'ORGANIZATION',
+  },
+  {
+    id: 2,
+    name: 'USERNAME',
+  },
+  {
+    id: 3,
+    name: 'EMAIL',
+  },
+  {
+    id: 4,
+    name: 'PHONE NUMBER',
+  },
+  {
+    id: 5,
+    name: 'DATE JOINED',
+  },
+  {
+    id: 6,
+    name: 'STATUS',
+  },
+];
+
+export const tableBodyContents = [
+  {
+    id: 1,
+    contents: [
+      { id: 1, name: 'Lendsqr' },
+      { id: 2, name: 'Adedeji' },
+      { id: 3, name: 'adedeji@lendsqr.com' },
+      { id: 4, name: '08078903721' },
+      { id: 5, name: 'May 15, 2020 10:00 AM' },
+      { id: 6, name: 'Inactive' },
+    ],
+  },
+  {
+    id: 2,
+    contents: [
+      { id: 1, name: 'Lendsqr' },
+      { id: 2, name: 'Adedeji' },
+      { id: 3, name: 'adedeji@lendsqr.com' },
+      { id: 4, name: '08078903721' },
+      { id: 5, name: 'May 15, 2020 10:00 AM' },
+      { id: 6, name: 'Blacklisted' },
+    ],
+  },
+  {
+    id: 3,
+    contents: [
+      { id: 1, name: 'Lendsqr' },
+      { id: 2, name: 'Adedeji' },
+      { id: 3, name: 'adedeji@lendsqr.com' },
+      { id: 4, name: '08078903721' },
+      { id: 5, name: 'May 15, 2020 10:00 AM' },
+      { id: 6, name: 'Pending' },
+    ],
+  },
+  {
+    id: 3,
+    contents: [
+      { id: 1, name: 'Lendsqr' },
+      { id: 2, name: 'Adedeji' },
+      { id: 3, name: 'adedeji@lendsqr.com' },
+      { id: 4, name: '08078903721' },
+      { id: 5, name: 'May 15, 2020 10:00 AM' },
+      { id: 6, name: 'Active' },
+    ],
+  },
+];
